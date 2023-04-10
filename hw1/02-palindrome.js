fc809a273eebdcd9856a2d018d698392891fbc7b
Richard Duffy <rduffy@pdx.edu>
2023-04-10 10:46:55 -0700

    Basically finished, need to just add some comments/formatting

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,16 @@
-const elem = document.querySelector('input');
+
+const elem = document.getElementById('inputNum');
 
 elem.addEventListener('input', handleInput);
+
+
+function handleInput(elem){
+    const val = document.querySelector('input').value;
+    console.log(val);
+    var reversedString = val.toString().split("").reverse().join("");
+    if (val == reversedString) {
+        document.getElementById("answer").innerHTML = "Yes.  This is a palindrome!";
+    } else {
+        document.getElementById("answer").innerHTML = "No.  Try again.";
+    }
+}
