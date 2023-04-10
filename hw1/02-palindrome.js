131328b808913e2342b63e859ff0ccbaefe17539
Richard Duffy <rduffy@pdx.edu>
2023-04-10 13:24:19 -0700

    added check for negative numbers

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -6,11 +6,16 @@ elem.addEventListener('input' , handleInput);
 
 function handleInput(elem){
     const val = document.querySelector('input').value;
-    console.log(val);
+   
     var reversedString = val.toString().split("").reverse().join("");
+    
     if (val == reversedString) {
         document.getElementById("answer").innerHTML = "Yes.  This is a palindrome!";
-    } else {
+    } else if (val !== reversedString && val >= 0) {
         document.getElementById("answer").innerHTML = "No.  Try again.";
     }
+    else if(val<0){
+        document.getElementById("answer").innerHTML = "Number must be greater then 0.";
+    }
+
 }
