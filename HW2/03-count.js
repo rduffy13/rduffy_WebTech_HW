1d6a94c8c692b4ab52d2bc6f0a3e3e4c5aba5964
Richard Duffy <rduffy@pdx.edu>
2023-04-25 12:22:08 -0700

    finished count and background

---
+++ HW2/03-count.js
@@ -0,0 +1,21 @@
+// Add your code here
+
+//input.addEventListener('keydown', handleKeyDown);
+
+//const elem = document.getElementById('word_find');
+//elem.addEventListener('keydown', get_word);
+
+//elem.addEventListener('input', get_word);
+
+const origin = document.getElementById("paragraph").innerHTML;
+
+function get_word(word){
+    const words = document.getElementById("paragraph").innerHTML;
+    const reg = new RegExp(`\\b${word}\\b`, 'gi');
+    const highlightedText = words.replace(reg, `<span style="background-color: yellow">${word}</span>`);
+    document.getElementById("paragraph").innerHTML = highlightedText;
+    if(!word){
+        document.getElementById("paragraph").innerHTML = origin;
+    }
+   
+}
