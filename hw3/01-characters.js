68b107457bc859fb7ae4c3b77afc955da2952e50
Richard Duffy <rduffy@pdx.edu>
2023-05-04 13:55:43 -0700

    finished exercise 1

--- hw3/01-characters.js
+++ hw3/01-characters.js
@@ -1,4 +1,36 @@
 // url for the Thrones API
 const url = 'https://thronesapi.com/api/v2/Characters';
 
-fetch(url);
+
+
+const characterList = document.getElementById('character-list');
+
+		fetch(url)
+			.then(response => response.json())
+			.then(characters => {
+				characters.forEach(character => {
+					const characterCard = document.createElement('div');
+					characterCard.className = 'character-card';
+
+					const characterImage = document.createElement('img');
+					characterImage.className = 'character-image';
+					characterImage.src = character.imageUrl;
+
+					const characterInfo = document.createElement('div');
+
+					const characterName = document.createElement('p');
+					characterName.textContent = character.fullName;
+
+					const characterTitle = document.createElement('p');
+					characterTitle.textContent = character.title;
+
+					characterInfo.appendChild(characterName);
+					characterInfo.appendChild(characterTitle);
+
+					characterCard.appendChild(characterImage);
+					characterCard.appendChild(characterInfo);
+
+					characterList.appendChild(characterCard);
+				});
+			})
+			.catch(error => console.log(error));
