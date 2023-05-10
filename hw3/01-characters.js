93027b2fa24c9c6137c19f2ee9ad10b5d904937b
Richard Duffy <rduffy@pdx.edu>
2023-05-10 12:44:23 -0700

    exer1 $ exer2

--- hw3/01-characters.js
+++ hw3/01-characters.js
@@ -1,4 +1,26 @@
 // url for the Thrones API
 const url = 'https://thronesapi.com/api/v2/Characters';
 
-fetch(url);
+const c_list = document.getElementById('charList');
+        fetch(url)
+            .then(response => response.json())
+            .then(characters => {
+                characters.forEach(character => {
+                    const singleChar = document.createElement('div');
+                    singleChar.className = 'single_char';
+                    const char_pic = document.createElement('img');
+                    char_pic.className = 'charPic';
+                    char_pic.src = character.imageUrl;
+                    const char_stats = document.createElement('div');
+                    const char_name = document.createElement('p');
+                    char_name.textContent = character.fullName;
+                    const char_title = document.createElement('p');
+                    char_title.textContent = character.title;
+                    char_stats.appendChild(char_name);
+                    char_stats.appendChild(char_title);
+                    singleChar.appendChild(char_pic);
+                    singleChar.appendChild(char_stats);
+                    c_list.appendChild(singleChar);
+                });
+            })
+            .catch(error => console.log(error));
--- hw3/02-charts.js
+++ hw3/02-charts.js
@@ -29,19 +29,50 @@ const borderColors = [
 // url for the Thrones API
 const url = 'https://thronesapi.com/api/v2/Characters';
 
-const renderChart = () => {
+
+async function get_info() {
+  const response = await fetch(url);
+  const char_group = await response.json();
+  return char_group;
+}
+async function renderChart() {
   const donutChart = document.querySelector('.donut-chart');
+  const char_list = await get_info();
+  const house_list = char_list.map(character => character.family);
+  const fam_totals = {};
+house_list.forEach(name => {
+  if (name in fam_totals) {
+    fam_totals[name]++;
+  } else {
+    fam_totals[name] = 1;
+  }
+});
+
+let chart_labels = [];
+let chart_data = [];
+
+Object.entries(fam_totals).forEach(([family, count], index) => {
+  chart_labels.push(`${family} (${count})`);
+  chart_data.push(count);
+});
+
+const colors = chart_labels.map(() => {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  return `rgba(${r}, ${g}, ${b}, 0.2)`;
+});
 
   new Chart(donutChart, {
     type: 'doughnut',
     data: {
-      labels: ['label', 'label', 'label', 'label'],
+      labels: chart_labels,
       datasets: [
         {
-          label: 'My First Dataset',
-          data: [1, 12, 33, 5],
-          backgroundColor: backgroundColors,
-          borderColor: borderColors,
+          label: 'G.O.T. Number of Characters by House',
+          data: chart_data,
+          backgroundColor: colors.map(color => color.replace('0.2', '1')),
+          borderColor: '#fff' ,
           borderWidth: 1,
         },
       ],
