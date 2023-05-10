ee8bde93352e522b558ca2481ad3dd1925adc16c
Richard Duffy <rduffy@pdx.edu>
2023-05-10 10:17:14 -0700

    finished doughnut chart

--- hw3/02-charts.js
+++ hw3/02-charts.js
@@ -1,3 +1,4 @@
+
 const backgroundColors = [
   'rgba(54, 162, 235, 0.8)',
   'rgba(255, 206, 86, 0.8)',
@@ -29,19 +30,59 @@ const borderColors = [
 // url for the Thrones API
 const url = 'https://thronesapi.com/api/v2/Characters';
 
-const renderChart = () => {
+
+async function getCharacters() {
+  const response = await fetch('https://thronesapi.com/api/v2/Characters');
+  const characters = await response.json();
+  return characters;
+}
+
+
+
+
+
+async function renderChart() {
   const donutChart = document.querySelector('.donut-chart');
+  const characters = await getCharacters();
+  const familyNames = characters.map(character => character.family);
+  const familyCounts = {};
+familyNames.forEach(name => {
+  if (name in familyCounts) {
+    familyCounts[name]++;
+  } else {
+    familyCounts[name] = 1;
+  }
+});
+
+
+const labels = [];
+const data = [];
+//const labels = Object.keys(familyCounts);
+//const data = Object.values(familyCounts);
+
+
+Object.entries(familyCounts).forEach(([family, count], index) => {
+  labels.push(`${family} (${count})`);
+  data.push(count);
+});
+
+const colors = labels.map(() => {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  return `rgba(${r}, ${g}, ${b}, 0.2)`;
+});
 
   new Chart(donutChart, {
     type: 'doughnut',
     data: {
-      labels: ['label', 'label', 'label', 'label'],
+      labels: labels,
       datasets: [
         {
-          label: 'My First Dataset',
-          data: [1, 12, 33, 5],
-          backgroundColor: backgroundColors,
-          borderColor: borderColors,
+          label: 'Game of Thrones Characters by House',
+          data: data,
+          backgroundColor: colors.map(color => color.replace('0.2', '1')),
+          borderColor: '#fff' ,
           borderWidth: 1,
         },
       ],
